8ebce09e91d36c6e601cafd8b296b3fb0e3b7f68
josec_velasquez <josec_velasquez@bantrab.net.gt>
2023-12-19 19:30:12 -0600

    ->react

---
+++ app-express-react.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+const colors = require('colors');
+
+const port = 8081;
+
+console.log('Inicia Programa Express');
+
+app.use(express.static('react'));
+
+app.get('*', function (req, res) {
+    //Respuesta dinamica
+    //res.send('404 | Page not found');
+
+    //Respuesta estática
+    res.sendFile(__dirname+'/public/404.html');
+});
+
+app.listen(port);
+
+console.log('Escuchando ',colors.cyan(port));
